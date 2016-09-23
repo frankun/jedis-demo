8691a8460817ff5a80a3642138ac6973f90cb286
frankun <836783712@qq.com>
2016-09-23 13:50:08 +0800

    jedis

---
+++ src/main/java/com/frankun/redisClient/Main.java
@@ -0,0 +1,10 @@
+package com.frankun.redisClient;
+
+/**
+ * Created by frankun on 2016/9/22.
+ */
+public class Main {
+    public static void main(String[] args){
+        new RedisClient().show();
+    }
+}
